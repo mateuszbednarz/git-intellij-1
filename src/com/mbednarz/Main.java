10f6b559982fe1de7a29ad6c738b070ebdb87458
Mateusz Bednarz <mateusz.bednarz@gmail.com>
2019-05-14 14:00:25 +0200

    Second sout line added

--- src/com/mbednarz/Main.java
+++ src/com/mbednarz/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Git integration test!");
+        System.out.println("Files were pushed to GitHub");
     }
 }
