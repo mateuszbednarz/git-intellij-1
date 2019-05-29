519f4f28a8caf71ec894420abbcde5de274edff2
Mateusz Bednarz <mateusz.bednarz@gmail.com>
2019-05-29 15:52:14 +0200

    Add new line
    Related with 'Git commit' command test in an IntelliJ IDE.

--- src/com/mbednarz/Main.java
+++ src/com/mbednarz/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Git integration test!");
         System.out.println("Files were pushed to GitHub");
+
+        System.out.println("Git commit using default editor test!");
     }
 }
