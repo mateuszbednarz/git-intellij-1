8dbd90f7db6d38de22ef00b56a25315206cca561
Mateusz Bednarz <mateusz.bednarz@gmail.com>
2019-06-04 17:09:31 +0200

    Add sources - ujopde - S2_L5

---
+++ src/com/mbednarz/ujopde/Komentarze.java
@@ -0,0 +1,32 @@
+package com.mbednarz.ujopde;/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+  @author Mateusz
+ */
+
+/* 
+
+
+fasfas
+fa
+sf
+asf
+a
+sf
+
+*/
+public class Komentarze {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //to jest komentarz jednoliniowy
+       
+    }
+    
+}
