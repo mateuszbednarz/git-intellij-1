925f3ff50bc589b950cdae4eed6bee7175ef9798
Mateusz Bednarz <mateusz.bednarz@gmail.com>
2019-06-04 17:48:11 +0200

    S2_L6 - ujopde sources & .gitignore update

--- src/com/mbednarz/ujopde/s2/S2_L6_Solution.java
+++ src/com/mbednarz/ujopde/s2/S2_L6_Solution.java
@@ -5,5 +5,6 @@ public class S2_L6_Solution
     public static void main(String[] args) 
     {
         System.out.println("To jest mój pierwszy program w Java");
+        System.out.println("Drugi raz");
     }
 }
