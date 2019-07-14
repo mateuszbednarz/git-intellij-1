dedf7052f23e3d6d0c5b41a808a58b5b6ab9312c
Mateusz Bednarz <mateusz.bednarz@gmail.com>
2019-07-14 15:15:36 +0200

    Add file OpAry

---
+++ src/com/mbednarz/ujopde/s5/OpAry.java
@@ -0,0 +1,22 @@
+package com.mbednarz.ujopde.s5;
+
+public class OpAry {
+
+    public static void main(String[] args) {
+
+        /*
+
+        OPERATOR - to znak służacy
+
+        OPERATORY ARYTMETYCZNE
+            + : dodawanie liczb oraz łaczenie string'ów
+            - : odejmowanie
+            * : mnożenie
+            / : dzielenie
+
+            = przypisanie wartości
+
+         */
+
+    }
+}
